6c05ada79e3b240a8aaebef6f9c9dae6ca528a40
k_avel <gilut_knight@hotmail.com>
2021-06-11 17:54:07 +0900

    android_controller >> 二日目 正常系の実装完了

--- android-auto-play-opencv-master/android_controller.py
+++ android-auto-play-opencv-master/android_controller.py
@@ -1,30 +1,51 @@
 # This Python file uses the following encoding: utf-8
 # pip install android-auto-play-opencv
-import android_auto_play_opencv as am
+import android_auto_play_opencv as a2p
 
-adbpath = '..\\platform-tools\\'
+adb_path = '..\\platform-tools\\'
+a2pmo = a2p.AapoManager(adb_path)
+
+template = {"org_quest_start": './template/quest_start.png',
+            "org_player_area": './template/quartet_4p.png',
+            "result_back": './template/result_back.png',
+            "result_next": './template/result_next.png'}
 
 
 def main():
-    aapo = am.AapoManager(adbpath)
-
     while True:
         # 画面キャプチャ
-        aapo.screencap()
+        a2pmo.screencap()
         # キャプチャした画像を保存
-        aapo.imgSave('./screenshot/screenshot.png')
+        # a2pmo.imgSave('./screenshot/screenshot.png')
+        state = check_state()
 
-        if check_participant():
-            quest_start(aapo)
+        if state == 'PT_OLG' and check_participant():
+            # 編成画面かつ出撃条件を満たす場合、出撃ボタンを押してみる。
+            # （入室しているプレイヤー全員が準備完了となるまで出撃不可）
+            quest_start()
+
+        else:
+
+            pass
+
+
+def check_state():
+    if a2pmo.chkImg(template.get("org_quest_start")):
+        return 'PT_OLG'
+    elif a2pmo.touchImg(template.get("result_next")) or a2pmo.touchImg(template.get("result_back")):
+        return 'RESULT'
+    else:
+        return 'BATTLE'
 
 
 def check_participant():
     # 参加者チェック
-    return True
+    # 4pが入室している場合
+    return not a2pmo.touchImg(template.get("org_player_area"))
 
 
-def quest_start(aapo):
-    aapo.touchImg('./template/quest_start.png')
+def quest_start():
+    a2pmo.touchImg(template.get("org_quest_start"))
 
 
 if __name__ == '__main__':
